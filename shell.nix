ed1cc31a64a176eb49113629cbb15d4a8ff88a23
Velnbur <kyrylo.baybula@distributedlab.com>
2024-03-31 19:33:03 +0300

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-p (import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
 in fetchTarball {
   url =
     lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
